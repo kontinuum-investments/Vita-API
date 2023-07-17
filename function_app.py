31e826ed4dc00c6cc410a9aeac6835b789da1ae9
Kavindu Athaudha <kavindu@k-ih.co.uk>
2023-07-17 22:23:42 +1200

    Test

---
+++ function_app.py
@@ -0,0 +1,25 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route="HttpTrigger")
+def HttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Stupid idiot.",
+             status_code=200
+        )
