4eed337808b695e1b42fb481d5d7e08375d51e1d
Kavindu Athaudha <kavindu@kih.com.sg>
2023-07-21 18:37:57 +1200

    VITA-1 Create Hades Azure Function

--- function_app.py
+++ function_app.py
@@ -1,3 +1,6 @@
+import datetime
+import logging
+
 import azure.functions as func
 from fastapi import FastAPI, Response
 
@@ -10,3 +13,13 @@ async def return_http_no_body():
 
 
 app = func.AsgiFunctionApp(app=fast_app, http_auth_level=func.AuthLevel.ANONYMOUS)
+
+
+@app.function_name(name="test_timer")
+@app.schedule(schedule="0 */5 * * * *", arg_name="mytimer", run_on_startup=True)
+def test_function(mytimer: func.TimerRequest) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+    if mytimer.past_due:
+        logging.info('The timer is past due!')
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
