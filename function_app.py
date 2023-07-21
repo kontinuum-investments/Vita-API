a8e87631921a8fc6b95497e550d6d65e911e3fff
Kavindu Athaudha <kavindu@kih.com.sg>
2023-07-21 18:26:14 +1200

    VITA-1 Create Hades Azure Function

--- function_app.py
+++ function_app.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, Request, Response
 fast_app = FastAPI()
 
 
+@fast_app.function_name(name="API")
 @fast_app.get("/return_http_no_body")
 async def return_http_no_body():
     return Response(content="Kavindu is handsome", media_type="text/plain")
