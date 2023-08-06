9c2d2afa805dea528cc7f9e9c9f3467264a062a0
Kavindu Athaudha <kavindu@kih.com.sg>
2023-08-06 20:48:06 +1200

    VITA-4 Develop Daily Finances Organization Job

--- function_app.py
+++ function_app.py
@@ -8,6 +8,7 @@ from api.ares.router import ares_router
 from api.athena.router import athena_router
 from api.chronos.router import chronos_router
 from api.hades.router import hades_router
+from api.hades.services import DailyFinances
 from api.hermes.router import hermes_router
 
 fast_app = FastAPI()
@@ -20,11 +21,7 @@ fast_app.include_router(hermes_router)
 app = func.AsgiFunctionApp(app=fast_app, http_auth_level=func.AuthLevel.ANONYMOUS)
 
 
-@app.function_name(name="test_timer")
-@app.schedule(schedule="0 */5 * * * *", arg_name="mytimer", run_on_startup=True)
-def test_function(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().replace(
-        tzinfo=datetime.timezone.utc).isoformat()
-    if mytimer.past_due:
-        logging.info('The timer is past due!')
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+@app.function_name(name="organize_daily_finances")
+@app.schedule(schedule="0 0 12 * * *", arg_name="mytimer", run_on_startup=True)
+async def organize_daily_finances(mytimer: func.TimerRequest) -> None:
+    await DailyFinances.do()
