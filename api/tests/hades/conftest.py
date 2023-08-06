ddbcdc87ba235cd0dac4202edc211a7a22d6e5f3
Kavindu Athaudha <kavindu@kih.com.sg>
2023-08-06 21:40:20 +1200

    VITA-4 Develop Daily Finances Organization Job

--- api/tests/hades/conftest.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from asyncio import AbstractEventLoop
-
-import pytest
-
-
-@pytest.fixture(scope="session")
-def event_loop() -> AbstractEventLoop:
-    return asyncio.get_event_loop()
