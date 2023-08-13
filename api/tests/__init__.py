dd14ff5d0b95c124989c143b342faadaedc11a16
Kavindu Athaudha <kavindu@kih.com.sg>
2023-08-14 10:06:20 +1200

    VITA-13 Set up tests in GitHub Actions
    Signed-off-by: Kavindu Athaudha <kavindu@kih.com.sg>

--- api/tests/__init__.py
+++ api/tests/__init__.py
@@ -4,22 +4,36 @@ from httpx import Response, AsyncClient, URL
 
 from main import app
 
+authentication_header: Dict[str, Any] = {"Authorization": "Bearer 1234"}
+
 
 async def get(url: str, **kwargs: Dict[str, Any]) -> Response:
+    if kwargs.get("headers") is None:
+        kwargs["headers"] = authentication_header
+
     async with AsyncClient(app=app, base_url="http://test") as ac:
         return await ac.get(URL(url), **kwargs)  # type: ignore[arg-type]
 
 
 async def put(url: str, **kwargs: Dict[str, Any]) -> Response:
+    if kwargs.get("headers") is None:
+        kwargs["headers"] = authentication_header
+
     async with AsyncClient(app=app, base_url="http://test") as ac:
         return await ac.put(URL(url), **kwargs)  # type: ignore[arg-type]
 
 
 async def post(url: str, **kwargs: Dict[str, Any]) -> Response:
+    if kwargs.get("headers") is None:
+        kwargs["headers"] = authentication_header
+
     async with AsyncClient(app=app, base_url="http://test") as ac:
         return await ac.post(URL(url), **kwargs)  # type: ignore[arg-type]
 
 
 async def delete(url: str, **kwargs: Dict[str, Any]) -> Response:
+    if kwargs.get("headers") is None:
+        kwargs["headers"] = authentication_header
+
     async with AsyncClient(app=app, base_url="http://test") as ac:
         return await ac.delete(URL(url), **kwargs)  # type: ignore[arg-type]
