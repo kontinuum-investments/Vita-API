2dc3efe05adbd77ca7c5295b6c512a27f05208b6
Kavindu Athaudha <kavindu@kih.com.sg>
2023-08-13 21:09:43 +1200

    CFC-10 Research about the possibility of Dockerization
    Signed-off-by: Kavindu Athaudha <kavindu@kih.com.sg>

--- api/tests/__init__.py
+++ api/tests/__init__.py
@@ -2,24 +2,24 @@ from typing import Dict, Any
 
 from httpx import Response, AsyncClient, URL
 
-from function_app import fast_app
+from main import app
 
 
 async def get(url: str, **kwargs: Dict[str, Any]) -> Response:
-    async with AsyncClient(app=fast_app, base_url="http://test") as ac:
+    async with AsyncClient(app=app, base_url="http://test") as ac:
         return await ac.get(URL(url), **kwargs)  # type: ignore[arg-type]
 
 
 async def put(url: str, **kwargs: Dict[str, Any]) -> Response:
-    async with AsyncClient(app=fast_app, base_url="http://test") as ac:
+    async with AsyncClient(app=app, base_url="http://test") as ac:
         return await ac.put(URL(url), **kwargs)  # type: ignore[arg-type]
 
 
 async def post(url: str, **kwargs: Dict[str, Any]) -> Response:
-    async with AsyncClient(app=fast_app, base_url="http://test") as ac:
+    async with AsyncClient(app=app, base_url="http://test") as ac:
         return await ac.post(URL(url), **kwargs)  # type: ignore[arg-type]
 
 
 async def delete(url: str, **kwargs: Dict[str, Any]) -> Response:
-    async with AsyncClient(app=fast_app, base_url="http://test") as ac:
+    async with AsyncClient(app=app, base_url="http://test") as ac:
         return await ac.delete(URL(url), **kwargs)  # type: ignore[arg-type]
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import azure.functions as func
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
@@ -8,20 +7,19 @@ from api.chronos.router import chronos_router
 from api.hades.router import hades_router
 from api.hermes.router import hermes_router
 
-fast_app = FastAPI()
-fast_app.include_router(ares_router)
-fast_app.include_router(athena_router)
-fast_app.include_router(chronos_router)
-fast_app.include_router(hades_router)
-fast_app.include_router(hermes_router)
+app = FastAPI()
+app.include_router(ares_router)
+app.include_router(athena_router)
+app.include_router(chronos_router)
+app.include_router(hades_router)
+app.include_router(hermes_router)
 
 
-@fast_app.get("/", include_in_schema=False)
+@app.get("/", include_in_schema=False)
 async def redirect_to_docs() -> RedirectResponse:
     return RedirectResponse(url="/docs")
 
-
-app = func.AsgiFunctionApp(app=fast_app, http_auth_level=func.AuthLevel.ANONYMOUS)
+# app = func.AsgiFunctionApp(app=fast_app, http_auth_level=func.AuthLevel.ANONYMOUS)
 
 # @app.function_name(name="daily_finances_organisation_job")
 # @app.schedule(schedule="0 0 12 * * *", arg_name="daily_finances_organisation_job")
