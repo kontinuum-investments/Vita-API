2427fc75d2c50882e6be5e1964b2fb97a794e458
Kavindu Athaudha <kavindu@kih.com.sg>
2023-08-13 23:01:50 +1200

    VITA-11 Set up Microsoft AD Authentication
    Signed-off-by: Kavindu Athaudha <kavindu@kih.com.sg>

--- api/ares/router.py
+++ api/ares/router.py
@@ -3,7 +3,7 @@ from sirius.iam.microsoft_entra_id import MicrosoftIdentityToken
 
 from api.ares import constants
 from api.ares.models import http
-from api.ares.services.microsoft_entra_id import MicrosoftEntraID, Scope
+from api.ares.services.microsoft_entra_id import MicrosoftEntraID
 from api.constants import ROUTE__ARES
 
 ares_router = APIRouter(prefix=ROUTE__ARES)
@@ -11,7 +11,7 @@ ares_router = APIRouter(prefix=ROUTE__ARES)
 
 @ares_router.post(constants.ROUTE__LOGIN)
 async def login(http_login: http.Login) -> MicrosoftIdentityToken:
-    return await MicrosoftEntraID.get_access_token(http_login.application_name, [Scope.USER_READ])
+    return await MicrosoftEntraID.get_access_token(http_login.application_name)
 
 
 @ares_router.post(constants.ROUTE__IS_ACCESS_TOKEN_VALID)
