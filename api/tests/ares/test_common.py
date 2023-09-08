22ea1504903e8aab8e6d6000f527123aaa7a938a
Kavindu Athaudha <kavindu@kih.com.sg>
2023-09-09 09:41:20 +1200

    AORTA-21 Create redundancy for determining if development environment
    Signed-off-by: Kavindu Athaudha <kavindu@kih.com.sg>

--- api/tests/ares/test_common.py
+++ api/tests/ares/test_common.py
@@ -7,8 +7,9 @@ from sirius.iam.microsoft_entra_id import MicrosoftIdentityToken
 
 from api.ares import constants
 from api.ares.models import http
+from api.ares.models.http import ConnectionInfo
 from api.constants import ROUTE__ARES
-from api.tests import post
+from api.tests import post, get
 
 
 @pytest.fixture(scope="session")
@@ -22,3 +23,10 @@ async def test_login() -> None:
     response: Response = await post(f"{ROUTE__ARES}{constants.ROUTE__LOGIN}", json=http.Login(application_name="Test Application").model_dump())
     microsoft_identity_token: MicrosoftIdentityToken = MicrosoftIdentityToken(**response.json())
     assert microsoft_identity_token.access_token is not None
+
+
+@pytest.mark.asyncio
+async def test_get_connection_data() -> None:
+    response: Response = await get(f"{ROUTE__ARES}{constants.ROUTE__GET_CONNECTION_INFO}")
+    connection_info: ConnectionInfo = ConnectionInfo(**response.json())
+    assert connection_info.client_ip is not None
