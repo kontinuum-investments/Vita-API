00a7d96a3a129a7beebb177c3b47eb171f902738
Kavindu Athaudha <kavindu@kih.com.sg>
2023-09-09 09:02:53 +1200

    AORTA-21 Create redundancy for determining if development environment
    Signed-off-by: Kavindu Athaudha <kavindu@kih.com.sg>

--- api/ares/router.py
+++ api/ares/router.py
@@ -1,7 +1,9 @@
 from typing import Annotated
 
+import sirius.common
 from fastapi import APIRouter, Depends, Request
 from sirius.iam.microsoft_entra_id import MicrosoftIdentityToken, MicrosoftIdentity
+from starlette.responses import JSONResponse
 
 from api.ares import constants
 from api.ares.models import http
@@ -23,3 +25,11 @@ async def login(http_login: http.Login) -> MicrosoftIdentityToken:
 @ares_router.get(constants.ROUTE__GET_CURRENT_USER)
 async def get_current_user(microsoft_identity: Annotated[MicrosoftIdentity, Depends(get_microsoft_identity)]) -> MicrosoftIdentity:
     return microsoft_identity
+
+
+@ares_router.get(constants.ROUTE__GET_DOMAIN_DATA)
+async def get_domain_data() -> JSONResponse:
+    return JSONResponse(
+        status_code=200,
+        content={"domain": sirius.common.get_servers_domain_name(), "fqdn": sirius.common.get_servers_fqdn()},
+    )
