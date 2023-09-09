4a4d3aa7c09d28a173e024e2dc57765dab6119f3
Kavindu Athaudha <kavindu@kih.com.sg>
2023-09-09 16:53:59 +1200

    VITA-22 Setup Wise Hooks
    Signed-off-by: Kavindu Athaudha <kavindu@kih.com.sg>

--- api/hades/models/__init__.py
+++ api/hades/models/__init__.py
@@ -2,7 +2,7 @@ import datetime
 from typing import Dict, Any
 
 from sirius import wise
-from sirius.common import DataClass
+from sirius.common import DataClass, Currency
 from sirius.wise import WiseAccount, WiseAccountType, PersonalProfile, CashAccount
 
 
@@ -21,3 +21,15 @@ class AccountCredit(DataClass):
                              is_attempted=request_data["data"]["current_state"] == "incoming_payment_waiting",
                              is_successful=request_data["data"]["current_state"] == "outgoing_payment_sent",
                              timestamp=request_data["data"]["occurred_at"])
+
+
+class AccountDebit(DataClass):
+    id: int
+    account: wise.CashAccount
+    timestamp: datetime.datetime
+
+    @staticmethod
+    def get_from_request_data(request_data: Dict[str, Any]) -> "AccountDebit":
+        personal_profile: PersonalProfile = WiseAccount.get(WiseAccountType.PRIMARY).personal_profile
+        cash_account: wise.CashAccount = personal_profile.get_cash_account(Currency(request_data["data"]["currency"]))
+        return AccountDebit(id=request_data["data"]["resource"]["id"], account=cash_account, timestamp=request_data["data"]["occurred_at"])
--- api/hades/services/wise_webhook.py
+++ api/hades/services/wise_webhook.py
@@ -4,7 +4,7 @@ from sirius.communication.discord import get_timestamp_string
 
 from api.athena.constants import DiscordTextChannel
 from api.athena.services.discord import Discord
-from api.hades.models import AccountCredit
+from api.hades.models import AccountCredit, AccountDebit
 
 
 class AccountUpdate:
@@ -12,8 +12,16 @@ class AccountUpdate:
     @classmethod
     async def handle_balance_update(cls, request_data: Dict[str, Any]) -> None:
         message_header: str = "**__Wise Account Update__**\n"
+
         if request_data["event_type"] == "transfers#state-change":
             account_credit: AccountCredit = AccountCredit.get_from_request_data(request_data)
             await Discord.send_message(DiscordTextChannel.WISE, f"{message_header}"
                                                                 f"Event: Account Credited\n"
                                                                 f"Timestamp: {get_timestamp_string(account_credit.timestamp)}")
+
+        elif request_data["event_type"] == "balances#credit":
+            account_debit: AccountDebit = AccountDebit.get_from_request_data(request_data)
+            await Discord.send_message(DiscordTextChannel.WISE, f"{message_header}"
+                                                                f"Event: Account Credited\n"
+                                                                f"Account: *{account_debit.account.currency.value}*"
+                                                                f"Timestamp: {get_timestamp_string(account_debit.timestamp)}")
