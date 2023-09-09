325c2db6d55aea68b3572b92192b03b2a7d95c79
Kavindu Athaudha <kavindu@kih.com.sg>
2023-09-09 15:41:23 +1200

    VITA-22 Setup Wise Hooks
    Signed-off-by: Kavindu Athaudha <kavindu@kih.com.sg>

--- api/hades/services/wise_webhook.py
+++ api/hades/services/wise_webhook.py
@@ -15,13 +15,20 @@ class AccountUpdate:
     async def handle_balance_update(cls, wise_web_hook: http.WiseWebHook) -> None:
         message_header: str = "**__Wise Account Update__**\n"
 
-        await Discord.send_message(DiscordTextChannel.WISE, message_header + str(wise_web_hook))
+        if wise_web_hook.event_type == WebhookAccountUpdateType.UPDATE:
+            amount: Decimal = wise_web_hook.data.amount if wise_web_hook.data.transaction_type == BalanceUpdateType.CREDIT else (wise_web_hook.data.amount * Decimal("-1"))
+            await Discord.send_message(DiscordTextChannel.WISE, message_header +
+                                       f"Event: Funds Moved\n"
+                                       f"Timestamp: {get_timestamp_string(wise_web_hook.data.occurred_at)}\n"
+                                       f"Account: {wise_web_hook.data.name}\n"
+                                       f"Amount: {wise_web_hook.data.currency.value} {get_decimal_str(amount)}\n"
+                                       f"Reference: {wise_web_hook.data.transfer_reference}")
 
-        # if wise_web_hook.event_type == WebhookAccountUpdateType.UPDATE:
-        #     amount: Decimal = wise_web_hook.data.amount if wise_web_hook.data.transaction_type == BalanceUpdateType.CREDIT else (wise_web_hook.data.amount * Decimal("-1"))
-        #     await Discord.send_message(DiscordTextChannel.WISE, message_header +
-        #                                f"Event: Funds Moved\n"
-        #                                f"Timestamp: {get_timestamp_string(wise_web_hook.data.occurred_at)}\n"
-        #                                f"Account: {wise_web_hook.data.name}\n"
-        #                                f"Amount: {wise_web_hook.data.currency.value} {get_decimal_str(amount)}\n"
-        #                                f"Reference: {wise_web_hook.data.transfer_reference}")
+        elif wise_web_hook.event_type == WebhookAccountUpdateType.CREDIT:
+            amount: Decimal = wise_web_hook.data.amount if wise_web_hook.data.transaction_type == BalanceUpdateType.CREDIT else (wise_web_hook.data.amount * Decimal("-1"))
+            await Discord.send_message(DiscordTextChannel.WISE, message_header +
+                                       f"Event: Funds Credited\n"
+                                       f"Timestamp: {get_timestamp_string(wise_web_hook.data.occurred_at)}\n"
+                                       f"Account: {wise_web_hook.data.name}\n"
+                                       f"Amount: {wise_web_hook.data.currency.value} {get_decimal_str(amount)}\n"
+                                       f"Reference: {wise_web_hook.data.transfer_reference}")
