2b66af872ecb5f19eccb32352346cb8da57f434c
Kavindu Athaudha <kavindu@kih.com.sg>
2023-09-09 17:16:43 +1200

    VITA-22 Setup Wise Hooks
    Signed-off-by: Kavindu Athaudha <kavindu@kih.com.sg>

--- api/hades/services/wise_webhook.py
+++ api/hades/services/wise_webhook.py
@@ -1,22 +1,23 @@
 from typing import Any, Dict
 
 from sirius.communication.discord import get_timestamp_string
+from sirius.wise import AccountCredit, AccountDebit
 
 from api.athena.constants import DiscordTextChannel
 from api.athena.services.discord import Discord
-from api.hades.models import AccountCredit, AccountDebit
 
 
 class AccountUpdate:
 
     @classmethod
     async def handle_balance_update(cls, request_data: Dict[str, Any]) -> None:
-        message_header: str = "**__Wise Account Update__**\n"
+        message_header: str = "**Wise Account Update**\n"
 
         if request_data["event_type"] == "transfers#state-change":
             account_credit: AccountCredit = AccountCredit.get_from_request_data(request_data)
             await Discord.send_message(DiscordTextChannel.WISE, f"{message_header}"
                                                                 f"Event: Account Credited\n"
+                                                                f"Account: *{account_credit.account.currency.value}*\n"
                                                                 f"Timestamp: {get_timestamp_string(account_credit.timestamp)}")
 
         elif request_data["event_type"] == "balances#credit":
