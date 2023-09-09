d82e11b6baa62c9e9ff84a3aff6caf844009e46e
Kavindu Athaudha <kavindu@kih.com.sg>
2023-09-09 17:44:21 +1200

    VITA-22 Setup Wise Hooks
    Signed-off-by: Kavindu Athaudha <kavindu@kih.com.sg>

--- api/hades/router.py
+++ api/hades/router.py
@@ -10,6 +10,7 @@ from api.athena.services.discord import Discord
 from api.constants import ROUTE__HADES
 from api.hades import constants
 from api.hades.models import http
+from api.hades.services.wise_webhook import AccountUpdate
 
 hades_router = APIRouter(prefix=ROUTE__HADES)
 
@@ -21,5 +22,8 @@ async def organize_daily_finances(microsoft_identity: Annotated[MicrosoftIdentit
 
 @hades_router.post(constants.ROUTE__WEBHOOK_WISE__ACCOUNT_UPDATE)
 async def webhook_account_update(request: Request) -> None:
-    await Discord.notify((await request.body()).decode("utf-8"))
-    # await AccountUpdate.handle_balance_update(await request.json())
+    try:
+        await AccountUpdate.handle_balance_update(await request.json())
+    except Exception as e:
+        await Discord.notify(f"**Unknown Account Update**\n"
+                             f"{(await request.body()).decode('utf-8')}")
