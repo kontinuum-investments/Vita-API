61cd87bd7527ab0d4eb4c78ab69bb6da78e38e27
Kavindu Athaudha <kavindu@kih.com.sg>
2023-09-10 22:59:27 +1200

    VITA-27 Create logic to disregard duplicate Wise Webhooks
    Signed-off-by: Kavindu Athaudha <kavindu@kih.com.sg>

---
+++ api/hades/models/database.py
@@ -0,0 +1,30 @@
+from typing import Union
+
+from sirius.database import DatabaseDocument
+from sirius.wise import AccountDebit, AccountCredit, WiseAccountType
+
+
+class WiseAccountUpdate(DatabaseDocument):
+    wise_id: int
+    wise_account_type: str
+
+    @staticmethod
+    def _get_from_account_update(wise_account_type: WiseAccountType, account_update: AccountDebit | AccountCredit) -> "WiseAccountUpdate":
+        return WiseAccountUpdate(wise_id=account_update.wise_id, wise_account_type="Primary" if wise_account_type.PRIMARY else "Secondary")
+
+    @classmethod
+    async def save_to_database(cls, wise_account_type: WiseAccountType, account_update: AccountDebit | AccountCredit | None) -> Union["WiseAccountUpdate", None]:
+        if account_update is None:
+            return None
+
+        wise_account_update: WiseAccountUpdate = cls._get_from_account_update(wise_account_type, account_update)
+        await wise_account_update.save()
+        return wise_account_update
+
+    @classmethod
+    async def is_duplicate(cls, wise_account_type: WiseAccountType, account_update: AccountDebit | AccountCredit | None) -> bool:
+        if account_update is None:
+            return False
+
+        wise_account_update: WiseAccountUpdate = cls._get_from_account_update(wise_account_type, account_update)
+        return len(await cls.find_by_query(wise_account_update)) > 0
--- api/hades/services/wise_webhook.py
+++ api/hades/services/wise_webhook.py
@@ -6,13 +6,19 @@ from sirius.wise import AccountCredit, AccountDebit, WiseWebhook, WiseAccount, W
 
 from api.athena.constants import DiscordTextChannel
 from api.athena.services.discord import Discord
+from api.hades.models.database import WiseAccountUpdate
 
 
 class AccountUpdate:
 
     @classmethod
     async def primary_account_update(cls, request_data: Dict[str, Any]) -> None:
-        account_update: AccountDebit | AccountCredit | None = await WiseWebhook.get_balance_update_object(request_data, WiseAccount.get(WiseAccountType.PRIMARY))
+        wise_account: WiseAccount = WiseAccount.get(WiseAccountType.PRIMARY)
+        account_update: AccountDebit | AccountCredit | None = await WiseWebhook.get_balance_update_object(request_data, wise_account)
+        if WiseAccountUpdate.is_duplicate(wise_account.type, account_update):
+            return
+
+        await WiseAccountUpdate.save_to_database(WiseAccountType.SECONDARY, account_update)
         if isinstance(account_update, AccountDebit):
             await Discord.send_message(DiscordTextChannel.WISE, f"**Primary Account Update**:\n"
                                                                 f"*Description*: Account Credited\n"
@@ -27,9 +33,16 @@ class AccountUpdate:
                                                                 f"*Balance*: {account_update.account.currency.value} {common.get_decimal_str(account_update.account_balance)}\n"
                                                                 f"*Timestamp*: {discord.get_timestamp_string(account_update.timestamp)}")
 
+        await WiseAccountUpdate.save_to_database(WiseAccountType.PRIMARY, account_update)
+
     @classmethod
     async def secondary_account_update(cls, request_data: Dict[str, Any]) -> None:
-        account_update: AccountDebit | AccountCredit | None = await WiseWebhook.get_balance_update_object(request_data, WiseAccount.get(WiseAccountType.PRIMARY))
+        wise_account: WiseAccount = WiseAccount.get(WiseAccountType.SECONDARY)
+        account_update: AccountDebit | AccountCredit | None = await WiseWebhook.get_balance_update_object(request_data, wise_account)
+        if WiseAccountUpdate.is_duplicate(wise_account.type, account_update):
+            return
+
+        await WiseAccountUpdate.save_to_database(WiseAccountType.SECONDARY, account_update)
         if isinstance(account_update, AccountDebit):
             await Discord.send_message(DiscordTextChannel.WISE, f"**Secondary Account Update**:\n"
                                                                 f"*Description*: Account Credited\n"
