78886077fc5788bc786f46b0bf0e665342a447f9
Kavindu Athaudha <kavindu@kih.com.sg>
2023-09-10 12:30:16 +1200

    VITA-26 Setup Wise Webhook for both Primary and Secondary Accounts
    Signed-off-by: Kavindu Athaudha <kavindu@kih.com.sg>

--- api/hades/services/wise_webhook.py
+++ api/hades/services/wise_webhook.py
@@ -2,7 +2,7 @@ from typing import Any, Dict
 
 from sirius import common
 from sirius.communication import discord
-from sirius.wise import AccountCredit, AccountDebit, WiseWebhook
+from sirius.wise import AccountCredit, AccountDebit, WiseWebhook, WiseAccount, WiseAccountType
 
 from api.athena.constants import DiscordTextChannel
 from api.athena.services.discord import Discord
@@ -12,12 +12,13 @@ class AccountUpdate:
 
     @classmethod
     async def primary_account_update(cls, request_data: Dict[str, Any]) -> None:
-        account_update: AccountDebit | AccountCredit | None = await WiseWebhook.get_balance_update_object(request_data)
+        account_update: AccountDebit | AccountCredit | None = await WiseWebhook.get_balance_update_object(request_data, WiseAccount.get(WiseAccountType.PRIMARY))
         if isinstance(account_update, AccountDebit):
             await Discord.send_message(DiscordTextChannel.WISE, f"**Primary Account Update**:\n"
                                                                 f"*Description*: Account Credited\n"
                                                                 f"*Timestamp*: {discord.get_timestamp_string(account_update.timestamp)}")
-        elif isinstance(account_update, AccountDebit):
+
+        elif isinstance(account_update, AccountDebit) and account_update.is_successful:
             await Discord.send_message(DiscordTextChannel.WISE, f"**Primary Account Update**:\n"
                                                                 f"*Description*: Account Debited\n"
                                                                 f"*Account*: {account_update.account.currency.value}\n"
@@ -28,12 +29,13 @@ class AccountUpdate:
 
     @classmethod
     async def secondary_account_update(cls, request_data: Dict[str, Any]) -> None:
-        account_update: AccountDebit | AccountCredit | None = await WiseWebhook.get_balance_update_object(request_data)
+        account_update: AccountDebit | AccountCredit | None = await WiseWebhook.get_balance_update_object(request_data, WiseAccount.get(WiseAccountType.PRIMARY))
         if isinstance(account_update, AccountDebit):
             await Discord.send_message(DiscordTextChannel.WISE, f"**Secondary Account Update**:\n"
                                                                 f"*Description*: Account Credited\n"
                                                                 f"*Timestamp*: {discord.get_timestamp_string(account_update.timestamp)}")
-        elif isinstance(account_update, AccountDebit):
+
+        elif isinstance(account_update, AccountDebit) and account_update.is_successful:
             await Discord.send_message(DiscordTextChannel.WISE, f"**Secondary Account Update**:\n"
                                                                 f"*Description*: Account Debited\n"
                                                                 f"*Account*: {account_update.account.currency.value}\n"
