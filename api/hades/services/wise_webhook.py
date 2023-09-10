c0d301e36a2b73e6d3b32bd12bc39617502824bd
Kavindu Athaudha <kavindu@kih.com.sg>
2023-09-10 23:42:31 +1200

    VITA-27 Create logic to disregard duplicate Wise Webhooks
    Signed-off-by: Kavindu Athaudha <kavindu@kih.com.sg>

--- api/hades/services/wise_webhook.py
+++ api/hades/services/wise_webhook.py
@@ -19,12 +19,12 @@ class AccountUpdate:
             return
 
         await WiseAccountUpdate.save_to_database(WiseAccountType.SECONDARY, account_update)
-        if isinstance(account_update, AccountDebit):
+        if isinstance(account_update, AccountDebit) and account_update.is_successful:
             await Discord.send_message(DiscordTextChannel.WISE, f"**Primary Account Update**:\n"
                                                                 f"*Description*: Account Credited\n"
                                                                 f"*Timestamp*: {discord.get_timestamp_string(account_update.timestamp)}")
 
-        elif isinstance(account_update, AccountCredit) and account_update.is_successful:
+        elif isinstance(account_update, AccountCredit):
             await Discord.send_message(DiscordTextChannel.WISE, f"**Primary Account Update**:\n"
                                                                 f"*Description*: Account Debited\n"
                                                                 f"*Account*: {account_update.account.currency.value}\n"
@@ -43,7 +43,7 @@ class AccountUpdate:
             return
 
         await WiseAccountUpdate.save_to_database(WiseAccountType.SECONDARY, account_update)
-        if isinstance(account_update, AccountDebit):
+        if isinstance(account_update, AccountDebit) and account_update.is_successful:
             await Discord.send_message(DiscordTextChannel.WISE, f"**Secondary Account Update**:\n"
                                                                 f"*Description*: Account Credited\n"
                                                                 f"*Timestamp*: {discord.get_timestamp_string(account_update.timestamp)}")
