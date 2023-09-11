50616e188c670699cedc3a1e84188cbaa03db679
Kavindu Athaudha <kavindu@kih.com.sg>
2023-09-11 18:37:34 +1200

    VITA-28 Fix bug where the Wise Webhook doesn't work for Balance Updates
    Signed-off-by: Kavindu Athaudha <kavindu@kih.com.sg>

--- api/hades/services/wise_webhook.py
+++ api/hades/services/wise_webhook.py
@@ -30,20 +30,20 @@ class AccountUpdate:
             return
 
         await WiseAccountUpdate.save_to_database(WiseAccountType.SECONDARY, account_update)
-        if isinstance(account_update, AccountDebit) and account_update.is_successful:
-            await Discord.send_message(DiscordTextChannel.WISE, f"**Primary Account Update**:\n"
-                                                                f"*Description*: Account Credited\n"
-                                                                f"*Timestamp*: {discord.get_timestamp_string(account_update.timestamp)}")
-
-        elif isinstance(account_update, AccountCredit):
+        if isinstance(account_update, AccountDebit):
             if account_update.is_successful:
                 await Discord.send_message(DiscordTextChannel.WISE, f"**Primary Account Update**:\n"
-                                                                    f"*Description*: Account Debited\n"
-                                                                    f"*Account*: {account_update.account.currency.value}\n"
-                                                                    f"*From*: {account_update.transaction.third_party}\n"
-                                                                    f"*Credited Amount*: {account_update.account.currency.value} {common.get_decimal_str(account_update.transaction.amount)}\n"
-                                                                    f"*Balance*: {account_update.account.currency.value} {common.get_decimal_str(account_update.account_balance)}\n"
+                                                                    f"*Description*: Account Credited\n"
                                                                     f"*Timestamp*: {discord.get_timestamp_string(account_update.timestamp)}")
+
+        elif isinstance(account_update, AccountCredit):
+            await Discord.send_message(DiscordTextChannel.WISE, f"**Primary Account Update**:\n"
+                                                                f"*Description*: Account Debited\n"
+                                                                f"*Account*: {account_update.account.currency.value}\n"
+                                                                f"*From*: {account_update.transaction.third_party}\n"
+                                                                f"*Credited Amount*: {account_update.account.currency.value} {common.get_decimal_str(account_update.transaction.amount)}\n"
+                                                                f"*Balance*: {account_update.account.currency.value} {common.get_decimal_str(account_update.account_balance)}\n"
+                                                                f"*Timestamp*: {discord.get_timestamp_string(account_update.timestamp)}")
         else:
             await Logger.debug(f"Unknown primary account update received:\n{json.dumps(request_data)}")
 
@@ -66,19 +66,19 @@ class AccountUpdate:
             return
 
         await WiseAccountUpdate.save_to_database(WiseAccountType.SECONDARY, account_update)
-        if isinstance(account_update, AccountDebit) and account_update.is_successful:
-            await Discord.send_message(DiscordTextChannel.WISE, f"**Secondary Account Update**:\n"
-                                                                f"*Description*: Account Credited\n"
-                                                                f"*Timestamp*: {discord.get_timestamp_string(account_update.timestamp)}")
-
-        elif isinstance(account_update, AccountCredit):
+        if isinstance(account_update, AccountDebit):
             if account_update.is_successful:
                 await Discord.send_message(DiscordTextChannel.WISE, f"**Secondary Account Update**:\n"
-                                                                    f"*Description*: Account Debited\n"
-                                                                    f"*Account*: {account_update.account.currency.value}\n"
-                                                                    f"*From*: {account_update.transaction.third_party}\n"
-                                                                    f"*Credited Amount*: {account_update.account.currency.value} {common.get_decimal_str(account_update.transaction.amount)}\n"
-                                                                    f"*Balance*: {account_update.account.currency.value} {common.get_decimal_str(account_update.account_balance)}\n"
+                                                                    f"*Description*: Account Credited\n"
                                                                     f"*Timestamp*: {discord.get_timestamp_string(account_update.timestamp)}")
+
+        elif isinstance(account_update, AccountCredit):
+            await Discord.send_message(DiscordTextChannel.WISE, f"**Secondary Account Update**:\n"
+                                                                f"*Description*: Account Debited\n"
+                                                                f"*Account*: {account_update.account.currency.value}\n"
+                                                                f"*From*: {account_update.transaction.third_party}\n"
+                                                                f"*Credited Amount*: {account_update.account.currency.value} {common.get_decimal_str(account_update.transaction.amount)}\n"
+                                                                f"*Balance*: {account_update.account.currency.value} {common.get_decimal_str(account_update.account_balance)}\n"
+                                                                f"*Timestamp*: {discord.get_timestamp_string(account_update.timestamp)}")
         else:
             await Logger.debug(f"Unknown secondary account update received:\n{json.dumps(request_data)}")
