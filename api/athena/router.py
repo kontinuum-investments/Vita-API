e307d7bd5bbdb69ba6e009380c3f03b492233603
Kavindu Athaudha <kavindu@kih.com.sg>
2023-09-22 12:10:29 +1200

    VITA-32 Setup Discord module to receive messages
    Signed-off-by: Kavindu Athaudha <kavindu@kih.com.sg>

--- api/athena/router.py
+++ api/athena/router.py
@@ -33,6 +33,6 @@ async def send_notification_message(microsoft_identity: Annotated[MicrosoftIdent
 @athena_router.put(constants.ROUTE__TEST_WEBHOOK)
 async def test_webhook(request: Request) -> None:
     message: str = f"**POST Request Received**\n" \
-                   f"Body: {await request.body()}\n" \
+                   f"Body: {(await request.body()).decode('utf-8')}\n" \
                    f"Headers: {str(request.headers)}"
     await Discord.notify(message)
