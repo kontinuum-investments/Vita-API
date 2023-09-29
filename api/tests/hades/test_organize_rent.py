f37aadb6e9966527fbf364a0be19b927f3cd439c
Kavindu Athaudha <kavindu@kih.com.sg>
2023-09-29 22:49:11 +1300

    VITA-33 Create Weekly Rent Job
    Signed-off-by: Kavindu Athaudha <kavindu@kih.com.sg>

--- api/tests/hades/test_organize_rent.py
+++ api/tests/hades/test_organize_rent.py
@@ -17,6 +17,18 @@ class TestOrganizeRent:
         organize_rent: OrganizeRent = await OrganizeRent.do()
         actual_tenant: Tenant = next(filter(lambda t: "kavindu" in t.name.lower(), organize_rent.tenant_list))
 
-        assert actual_tenant.is_sufficient_funds
+        assert not actual_tenant.is_sufficient_funds
         assert actual_tenant.amount_needed == Decimal("1")
         assert actual_tenant.rent_paid_until == datetime.date.today()
+
+    @pytest.mark.asyncio
+    async def test_sufficient_funds(self) -> None:
+        wise_account: WiseAccount = WiseAccount.get(WiseAccountType.PRIMARY)
+        tenant: Tenant = next(filter(lambda t: "kavindu" in t.name.lower(), Tenant.get_all(wise_account)))
+        await tenant.reserve_account._set_balance(tenant.rent * Decimal(4))
+        organize_rent: OrganizeRent = await OrganizeRent.do()
+        actual_tenant: Tenant = next(filter(lambda t: "kavindu" in t.name.lower(), organize_rent.tenant_list))
+
+        assert actual_tenant.is_sufficient_funds
+        assert actual_tenant.amount_needed == Decimal("0")
+        assert actual_tenant.rent_paid_until == (datetime.date.today() + datetime.timedelta(weeks=4))
