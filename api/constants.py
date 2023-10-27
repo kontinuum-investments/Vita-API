785e2346761ff6e4191dcba0bbd6a1f070a752ce
Kavindu Athaudha <kavindu@kih.com.sg>
2023-10-28 09:28:37 +1300

    Change localhost port to 5000

--- api/constants.py
+++ api/constants.py
@@ -1,6 +1,6 @@
 from sirius import common
 
-BASE_URL: str = "http://localhost" if common.is_development_environment() else f"https://vita-api{'-test' if common.is_test_environment() else ''}.kih.com.sg"
+BASE_URL: str = "http://localhost:5000" if common.is_development_environment() else f"https://vita-api{'-test' if common.is_test_environment() else ''}.kih.com.sg"
 ROUTE__ARES: str = "/ares"
 ROUTE__ATHENA: str = "/athena"
 ROUTE__CHRONOS: str = "/chronos"
