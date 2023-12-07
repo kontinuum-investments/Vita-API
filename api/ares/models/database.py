c06bdcab87dfae4da143f2b16e1bf9bc2b2b908a
Kavindu Athaudha <kavindu@kih.com.sg>
2023-12-08 11:46:34 +1300

    VITA-46 Save all requests and responses
    Signed-off-by: Kavindu Athaudha <kavindu@kih.com.sg>

--- api/ares/models/database.py
+++ api/ares/models/database.py
@@ -35,6 +35,12 @@ class HTTPExchange(DatabaseDocument):
         if common.is_development_environment():
             return
 
+        await HTTPExchange(request=HTTPExchange._get_request(fast_api_request),
+                           response=HTTPExchange._get_response(fast_api_response),
+                           timestamp=datetime.datetime.utcnow()).save()
+
+    @staticmethod
+    async def _get_response(fast_api_response: StreamingResponse) -> Response:
         response_body_bytes_list: List[bytes] = [chunk async for chunk in fast_api_response.body_iterator]  # type:ignore[misc]
         fast_api_response.body_iterator = iterate_in_threadpool(iter(response_body_bytes_list))
         response_body_string: str = (b''.join(response_body_bytes_list)).decode()
@@ -44,19 +50,25 @@ class HTTPExchange(DatabaseDocument):
         except JSONDecodeError:
             response_body = response_body_string if response_body_string != "" and response_body_string != "null" else None
 
-        response: Response = Response(body=response_body, headers=dict(fast_api_response.headers))
+        return Response(body=response_body, headers=dict(fast_api_response.headers))
+
+    @staticmethod
+    async def _get_request(fast_api_request: requests.Request) -> Request:
         request_query_params: Dict[str, Any] = dict(fast_api_request.query_params)
         request_body_string: str = (await fast_api_request.body()).decode("UTF-8")
+
+        async def get_body() -> bytes:
+            return request_body_string.encode()
+
+        fast_api_request.body = get_body  # type:ignore[method-assign]
 
         try:
             request_body: Dict[str, Any] | str | None = json.loads(request_body_string)
         except JSONDecodeError:
             request_body = request_body_string if request_body_string != "" and request_body_string != "null" else None
 
-        request: Request = Request(query_params=request_query_params if len(request_query_params) > 0 else None,
-                                   body=request_body,
-                                   headers=dict(fast_api_request.headers),
-                                   ip_address=fast_api_request.get("client")[0],
-                                   port_number=fast_api_request.get("client")[1])
-
-        await HTTPExchange(request=request, response=response, timestamp=datetime.datetime.utcnow()).save()
+        return Request(query_params=request_query_params if len(request_query_params) > 0 else None,
+                       body=request_body,
+                       headers=dict(fast_api_request.headers),
+                       ip_address=fast_api_request.get("client")[0],
+                       port_number=fast_api_request.get("client")[1])
