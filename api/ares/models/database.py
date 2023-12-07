9d94de219f5cc25e7829acd942fe2544006a492e
Kavindu Athaudha <kavindu@kih.com.sg>
2023-12-07 15:02:25 +1300

    VITA-46 Save all requests and responses
    Signed-off-by: Kavindu Athaudha <kavindu@kih.com.sg>

--- api/ares/models/database.py
+++ api/ares/models/database.py
@@ -1,6 +1,7 @@
 import asyncio
 import datetime
 import json
+from json import JSONDecodeError
 from typing import Dict, Any, List
 
 import fastapi
@@ -10,11 +11,9 @@ from sirius.database import DatabaseDocument
 from starlette.concurrency import iterate_in_threadpool
 from starlette.responses import StreamingResponse
 
-from api.athena.services.discord import Discord
-
 
 class Response(DataClass):
-    body: Dict[str, Any]
+    body: Dict[str, Any] | str | None
     headers: Dict[str, Any]
 
 
@@ -37,22 +36,23 @@ class HTTPExchange(DatabaseDocument):
         if common.is_development_environment():
             return
 
-        response_body: List[bytes] = [chunk async for chunk in fast_api_response.body_iterator]  # type:ignore[misc]
-        fast_api_response.body_iterator = iterate_in_threadpool(iter(response_body))
-        response_body_string: str = (b''.join(response_body)).decode()
-        fast_api_response.body_iterator = iterate_in_threadpool(iter(response_body))
-        response: Response = Response(body=json.loads(response_body_string) if response_body_string != "" else None,
-                                      headers=dict(fast_api_response.headers))
+        response_body_bytes_list: List[bytes] = [chunk async for chunk in fast_api_response.body_iterator]  # type:ignore[misc]
+        fast_api_response.body_iterator = iterate_in_threadpool(iter(response_body_bytes_list))
+        response_body_string: str = (b''.join(response_body_bytes_list)).decode()
+        fast_api_response.body_iterator = iterate_in_threadpool(iter(response_body_bytes_list))
+        try:
+            response_body: Dict[str, Any] | str | None = json.loads(response_body_string)
+        except JSONDecodeError:
+            response_body = response_body_string if response_body_string != "" and response_body_string != "null" else None
 
+        response: Response = Response(body=response_body, headers=dict(fast_api_response.headers))
         request_query_params: Dict[str, Any] = dict(fast_api_request.query_params)
         request_body_string: str = (await fast_api_request.body()).decode("UTF-8")
 
         try:
             request_body: Dict[str, Any] | str | None = json.loads(request_body_string)
-        except Exception:
-            request_body = request_body_string
-
-        await Discord.notify(f"Request Body String:\n{request_body_string}")
+        except JSONDecodeError:
+            request_body = request_body_string if request_body_string != "" and request_body_string != "null" else None
 
         request: Request = Request(query_params=request_query_params if len(request_query_params) > 0 else None,
                                    body=request_body,
