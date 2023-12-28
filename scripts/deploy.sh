faf1e8c7156f0df983f1e387cd7890416394a9fc
Kavindu Athaudha <kavindu@kih.com.sg>
2023-12-28 20:58:23 +1300

    CFC-11 Migrate Architecture to Bare Metal
    Signed-off-by: Kavindu Athaudha <kavindu@kih.com.sg>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,12 @@
 
 # Variables
 container_name="Vita-API"
+tag=""
+if [ "$ENVIRONMENT" == "Production" ]; then
+    export tag="latest"
+else
+    export tag="dev"
+fi
 
 # Set the environmental variables
 ENV_VARS="ENVIRONMENT=$ENVIRONMENT"
@@ -12,7 +18,7 @@ ENV_VARS="$ENV_VARS,AZURE_KEY_VAULT_URL=\"$AZURE_KEY_VAULT_URL\""
 
 # Set the command to start the microservice
 command="source <(curl -s https://raw.githubusercontent.com/kontinuum-investments/Central-Finite-Curve/production/citadel/scripts/library.sh) && \
-  deploy_container $DOCKERHUB_USERNAME/$DOCKERHUB_REPOSITORY:latest $container_name $CONTAINER_PORT $HOST_PORT $ENV_VARS"
+  deploy_container $DOCKERHUB_USERNAME/$DOCKERHUB_REPOSITORY:$tag $container_name $CONTAINER_PORT $HOST_PORT $ENV_VARS"
 
 # SSH into the Citadel
 temp_key_file=$(mktemp)
