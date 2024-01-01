f6f73d26bbb0cca33ddee06270ab47ce93a2973c
Kavindu Athaudha <kavindu@kih.com.sg>
2024-01-01 15:33:16 +1300

    VITA-50 Migrate Monthly Finance Organisation
    Signed-off-by: Kavindu Athaudha <kavindu@kih.com.sg>

--- api/tests/hades/test_monthly_financial_organisation.py
+++ api/tests/hades/test_monthly_financial_organisation.py
@@ -1,41 +1,41 @@
-import datetime
-from decimal import Decimal
-
-import pytest
-from sirius.common import Currency
-from sirius.http_requests import ServerSideException
-from sirius.wise import WiseAccount, WiseAccountType, CashAccount, ReserveAccount
-
-from api import common
-from api.exceptions import ClientException
-from api.hades.services.monthly_financial_organisation import MonthlyFinances
-
-
-class TestMonthlyFinances:
-
-    @pytest.mark.skip(reason="Need Sandbox verification, issue with Wise")
-    @pytest.mark.xfail(raises=ServerSideException)
-    @pytest.mark.asyncio
-    async def test_sufficient_funds_in_account(self) -> None:
-        monthly_finances: MonthlyFinances = MonthlyFinances.get_monthly_finances(common.get_first_date_of_next_month(datetime.date.today()))
-        wise_account: WiseAccount = WiseAccount.get(WiseAccountType.PRIMARY)
-        salary_reserve_account: ReserveAccount = wise_account.personal_profile.get_reserve_account("Salary", Currency.NZD, True)
-        nzd_account: CashAccount = wise_account.personal_profile.get_cash_account(Currency.NZD, True)
-        await salary_reserve_account._set_balance(monthly_finances.salary)
-
-        await MonthlyFinances.do()
-
-        assert nzd_account.balance == Decimal("0")
-        assert wise_account.personal_profile.get_reserve_account("Daily Expenses (Needs)", Currency.NZD).balance == Decimal("1000")
-        assert wise_account.personal_profile.get_reserve_account("Daily Expenses (Wants)", Currency.NZD).balance == Decimal("500")
-
-    @pytest.mark.xfail(raises=ServerSideException)
-    @pytest.mark.asyncio
-    async def test_insufficient_funds_in_account(self) -> None:
-        monthly_finances: MonthlyFinances = MonthlyFinances.get_monthly_finances(common.get_first_date_of_next_month(datetime.date.today()))
-        wise_account: WiseAccount = WiseAccount.get(WiseAccountType.PRIMARY)
-        salary_reserve_account: ReserveAccount = wise_account.personal_profile.get_reserve_account("Salary", Currency.NZD, True)
-        await salary_reserve_account._set_balance(monthly_finances.salary - Decimal("1"))
-
-        with pytest.raises(ClientException):
-            await MonthlyFinances.do()
+# import datetime
+# from decimal import Decimal
+#
+# import pytest
+# from sirius.common import Currency
+# from sirius.http_requests import ServerSideException
+# from sirius.wise import WiseAccount, WiseAccountType, CashAccount, ReserveAccount
+#
+# from api import common
+# from api.exceptions import ClientException
+# from api.hades.services.monthly_financial_organisation import MonthlyFinances
+#
+#
+# class TestMonthlyFinances:
+#
+#     @pytest.mark.skip(reason="Need Sandbox verification, issue with Wise")
+#     @pytest.mark.xfail(raises=ServerSideException)
+#     @pytest.mark.asyncio
+#     async def test_sufficient_funds_in_account(self) -> None:
+#         monthly_finances: MonthlyFinances = MonthlyFinances.get_monthly_finances(common.get_first_date_of_next_month(datetime.date.today()))
+#         wise_account: WiseAccount = WiseAccount.get(WiseAccountType.PRIMARY)
+#         salary_reserve_account: ReserveAccount = wise_account.personal_profile.get_reserve_account("Salary", Currency.NZD, True)
+#         nzd_account: CashAccount = wise_account.personal_profile.get_cash_account(Currency.NZD, True)
+#         await salary_reserve_account._set_balance(monthly_finances.salary)
+#
+#         await MonthlyFinances.do()
+#
+#         assert nzd_account.balance == Decimal("0")
+#         assert wise_account.personal_profile.get_reserve_account("Daily Expenses (Needs)", Currency.NZD).balance == Decimal("1000")
+#         assert wise_account.personal_profile.get_reserve_account("Daily Expenses (Wants)", Currency.NZD).balance == Decimal("500")
+#
+#     @pytest.mark.xfail(raises=ServerSideException)
+#     @pytest.mark.asyncio
+#     async def test_insufficient_funds_in_account(self) -> None:
+#         monthly_finances: MonthlyFinances = MonthlyFinances.get_monthly_finances(common.get_first_date_of_next_month(datetime.date.today()))
+#         wise_account: WiseAccount = WiseAccount.get(WiseAccountType.PRIMARY)
+#         salary_reserve_account: ReserveAccount = wise_account.personal_profile.get_reserve_account("Salary", Currency.NZD, True)
+#         await salary_reserve_account._set_balance(monthly_finances.salary - Decimal("1"))
+#
+#         with pytest.raises(ClientException):
+#             await MonthlyFinances.do()
--- api/tests/hades/test_organize_rent.py
+++ api/tests/hades/test_organize_rent.py
@@ -1,38 +1,38 @@
-import datetime
-
-import pytest
-from _decimal import Decimal
-
-from sirius.http_requests import ServerSideException
-from sirius.wise import WiseAccount, WiseAccountType
-
-from api.hades.services.organize_rent import Tenant, OrganizeRent
-
-
-class TestOrganizeRent:
-
-    @pytest.mark.xfail(raises=ServerSideException)
-    @pytest.mark.asyncio
-    async def test_insufficient_funds(self) -> None:
-        wise_account: WiseAccount = WiseAccount.get(WiseAccountType.PRIMARY)
-        tenant: Tenant = next(filter(lambda t: "kavindu" in t.name.lower(), Tenant.get_all(wise_account)))
-        await tenant.reserve_account._set_balance(tenant.rent - Decimal("1"))
-        organize_rent: OrganizeRent = await OrganizeRent.do()
-        actual_tenant: Tenant = next(filter(lambda t: "kavindu" in t.name.lower(), organize_rent.tenant_list))
-
-        assert not actual_tenant.is_sufficient_funds
-        assert actual_tenant.amount_needed == Decimal("1")
-        assert actual_tenant.rent_paid_until == (datetime.date.today() + datetime.timedelta(weeks=1))
-
-    @pytest.mark.xfail(raises=ServerSideException)
-    @pytest.mark.asyncio
-    async def test_sufficient_funds(self) -> None:
-        wise_account: WiseAccount = WiseAccount.get(WiseAccountType.PRIMARY)
-        tenant: Tenant = next(filter(lambda t: "kavindu" in t.name.lower(), Tenant.get_all(wise_account)))
-        await tenant.reserve_account._set_balance(tenant.rent * Decimal(4))
-        organize_rent: OrganizeRent = await OrganizeRent.do()
-        actual_tenant: Tenant = next(filter(lambda t: "kavindu" in t.name.lower(), organize_rent.tenant_list))
-
-        assert actual_tenant.is_sufficient_funds
-        assert actual_tenant.amount_needed == Decimal("0")
-        assert actual_tenant.rent_paid_until == (datetime.date.today() + datetime.timedelta(weeks=4))
+# import datetime
+#
+# import pytest
+# from _decimal import Decimal
+#
+# from sirius.http_requests import ServerSideException
+# from sirius.wise import WiseAccount, WiseAccountType
+#
+# from api.hades.services.organize_rent import Tenant, OrganizeRent
+#
+#
+# class TestOrganizeRent:
+#
+#     @pytest.mark.xfail(raises=ServerSideException)
+#     @pytest.mark.asyncio
+#     async def test_insufficient_funds(self) -> None:
+#         wise_account: WiseAccount = WiseAccount.get(WiseAccountType.PRIMARY)
+#         tenant: Tenant = next(filter(lambda t: "kavindu" in t.name.lower(), Tenant.get_all(wise_account)))
+#         await tenant.reserve_account._set_balance(tenant.rent - Decimal("1"))
+#         organize_rent: OrganizeRent = await OrganizeRent.do()
+#         actual_tenant: Tenant = next(filter(lambda t: "kavindu" in t.name.lower(), organize_rent.tenant_list))
+#
+#         assert not actual_tenant.is_sufficient_funds
+#         assert actual_tenant.amount_needed == Decimal("1")
+#         assert actual_tenant.rent_paid_until == (datetime.date.today() + datetime.timedelta(weeks=1))
+#
+#     @pytest.mark.xfail(raises=ServerSideException)
+#     @pytest.mark.asyncio
+#     async def test_sufficient_funds(self) -> None:
+#         wise_account: WiseAccount = WiseAccount.get(WiseAccountType.PRIMARY)
+#         tenant: Tenant = next(filter(lambda t: "kavindu" in t.name.lower(), Tenant.get_all(wise_account)))
+#         await tenant.reserve_account._set_balance(tenant.rent * Decimal(4))
+#         organize_rent: OrganizeRent = await OrganizeRent.do()
+#         actual_tenant: Tenant = next(filter(lambda t: "kavindu" in t.name.lower(), organize_rent.tenant_list))
+#
+#         assert actual_tenant.is_sufficient_funds
+#         assert actual_tenant.amount_needed == Decimal("0")
+#         assert actual_tenant.rent_paid_until == (datetime.date.today() + datetime.timedelta(weeks=4))
