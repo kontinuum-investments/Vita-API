f5631597e820465acabe3f57347d73e57df76a59
Kavindu Athaudha <kavindu@kih.com.sg>
2024-01-03 22:07:14 +1300

    VITA-51 Migrate Daily Finance Organisation Job to v2

--- api/hades/services/transaction_organisation/__init__.py
+++ api/hades/services/transaction_organisation/__init__.py
@@ -47,16 +47,16 @@ class AccountUpdate:
 class WiseCreditEvent:
     @staticmethod
     async def do(account_credit: AccountCredit) -> None:
-        expected_recipient: ExpectedRecipient | None = ExpectedRecipient.get(account_credit)
-        await account_credit.account.transfer(expected_recipient.reserve_account, account_credit.transaction.amount)
-        if expected_recipient.notification_phone_number is not None:
-            asyncio.ensure_future(sms.send_message(expected_recipient.notification_phone_number, f"A fund transfer of {account_credit.account.currency.value} {common.get_decimal_str(account_credit.transaction.amount)} has been "
+        expected_remitter: ExpectedRemitter | None = ExpectedRemitter.get(account_credit)
+        await account_credit.account.transfer(expected_remitter.reserve_account, account_credit.transaction.amount)
+        if expected_remitter.notification_phone_number is not None:
+            asyncio.ensure_future(sms.send_message(expected_remitter.notification_phone_number, f"A fund transfer of {account_credit.account.currency.value} {common.get_decimal_str(account_credit.transaction.amount)} has been "
                                                                                                  f"received.\nThis is an automated message from Athena."))
 
         asyncio.ensure_future(Discord.send_message(DiscordTextChannel.WISE, f"**Account Credited**\n"
-                                                                            f"*From*: {account_credit.transaction.third_party if expected_recipient.is_unknown_recipient else expected_recipient.recipient_name}\n"
+                                                                            f"*From*: {account_credit.transaction.third_party if expected_remitter.is_unknown_recipient else expected_remitter.recipient_name}\n"
                                                                             f"*Amount*: {account_credit.account.currency.value} {common.get_decimal_str(account_credit.transaction.amount)}\n"
-                                                                            f"*Reserve Account Balance*: {account_credit.account.currency.value} {common.get_decimal_str(expected_recipient.reserve_account.balance)}"))
+                                                                            f"*Reserve Account Balance*: {account_credit.account.currency.value} {common.get_decimal_str(expected_remitter.reserve_account.balance)}"))
 
         FinancesSettings.notify_if_only_cash_reserve_amount_present(account_credit.account.profile.wise_account)
 
@@ -67,7 +67,7 @@ class WiseDebitEvent:
         FinancesSettings.notify_if_only_cash_reserve_amount_present(account_debit.account.profile.wise_account)
 
 
-class ExpectedRecipient(DataClass):
+class ExpectedRemitter(DataClass):
     recipient_name: str | None
     name_in_statement: str | None
     reserve_account: ReserveAccount
@@ -75,15 +75,15 @@ class ExpectedRecipient(DataClass):
     is_unknown_recipient: bool = False
 
     @staticmethod
-    def get(account_credit: AccountCredit) -> Optional["ExpectedRecipient"]:
-        expected_recipient: ExpectedRecipient | None = None
+    def get(account_credit: AccountCredit) -> Optional["ExpectedRemitter"]:
+        expected_remitter: ExpectedRemitter | None = None
         if isinstance(account_credit.transaction.third_party, str):
             name_in_statement: str = account_credit.transaction.third_party.split(" |")[0]
-            expected_recipient = ExpectedRecipient._get_by_name_in_statement(name_in_statement, account_credit.account.profile.wise_account)
+            expected_remitter = ExpectedRemitter._get_by_name_in_statement(name_in_statement, account_credit.account.profile.wise_account)
 
-        if expected_recipient is None:
+        if expected_remitter is None:
             reserve_account: ReserveAccount = account_credit.account.profile.wise_account.personal_profile.get_reserve_account(f"Unknown [Reserve]", account_credit.account.currency, True)
-            expected_recipient = ExpectedRecipient(
+            expected_remitter = ExpectedRemitter(
                 recipient_name=None,
                 name_in_statement=None,
                 reserve_account=reserve_account,
@@ -91,32 +91,32 @@ class ExpectedRecipient(DataClass):
                 notification_phone_number=None
             )
 
-        return expected_recipient
+        return expected_remitter
 
     @staticmethod
-    def get_all(wise_account: WiseAccount | None = None) -> List["ExpectedRecipient"]:
+    def get_all(wise_account: WiseAccount | None = None) -> List["ExpectedRemitter"]:
         wise_account = WiseAccount.get(WiseAccountType.PRIMARY) if wise_account is None else wise_account
         excel_file_path: str = FinancesSettings.get_monthly_finances_excel_file_path()
-        raw_recipient_list: List[Dict[Any, Any]] = excel.get_excel_data(excel_file_path, "Expected Recipients")
-        expected_recipient_list: List[ExpectedRecipient] = []
+        raw_remitter_list: List[Dict[Any, Any]] = excel.get_excel_data(excel_file_path, "Expected Remitters")
+        expected_remitter_list: List[ExpectedRemitter] = []
 
-        for raw_recipient in raw_recipient_list:
-            recipient_name: str = raw_recipient["Recipient Name"]
-            currency: Currency = Currency(raw_recipient["Currency"])
+        for raw_remitter in raw_remitter_list:
+            recipient_name: str = raw_remitter["Remitter's Name"]
+            currency: Currency = Currency(raw_remitter["Currency"])
             reserve_account: ReserveAccount = wise_account.personal_profile.get_reserve_account(f"{recipient_name} [Reserve]", currency, True)
 
-            expected_recipient_list.append(ExpectedRecipient(
+            expected_remitter_list.append(ExpectedRemitter(
                 recipient_name=recipient_name,
-                name_in_statement=raw_recipient["Name in Statement"],
+                name_in_statement=raw_remitter["Name in Statement"],
                 reserve_account=reserve_account,
-                notification_phone_number=raw_recipient["Notification Phone Number"]
+                notification_phone_number=raw_remitter["Notification Phone Number"]
             ))
 
-        return expected_recipient_list
+        return expected_remitter_list
 
     @staticmethod
-    def _get_by_name_in_statement(name_in_statement: str, wise_account: WiseAccount) -> Optional["ExpectedRecipient"]:
+    def _get_by_name_in_statement(name_in_statement: str, wise_account: WiseAccount) -> Optional["ExpectedRemitter"]:
         try:
-            return next(filter(lambda e: e.name_in_statement.lower() == name_in_statement.lower(), ExpectedRecipient.get_all(wise_account)))
+            return next(filter(lambda e: e.name_in_statement.lower() == name_in_statement.lower(), ExpectedRemitter.get_all(wise_account)))
         except StopIteration:
             return None
