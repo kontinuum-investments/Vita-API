1edd2dc7b855802d3aa3765ac6648ac4db8858ee
Kavindu Athaudha <kavindu@kih.com.sg>
2025-08-25 17:27:39 +1200

    Added security.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+kubectl delete -f deploy.yml || kubectl delete pods --all || true
+
 kubectl apply -f config-map.yml
-kubectl delete -f deploy.yml || true
 kubectl apply -f deploy.yml
 
 kubectl wait --for=condition=ready pod -l app=citadel --timeout=300s
--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from sirius import common
 
 from tools import discord, ibkr, wise
 
+
+def verify_token(token: HTTPAuthorizationCredentials = Depends(HTTPBearer())) -> None:
+    api_key: str = common.get_environmental_secret("API_KEY")
+    if token.credentials != api_key:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+
 app = FastAPI()
-app.include_router(discord.router, prefix="/discord")
-app.include_router(ibkr.router, prefix="/ibkr")
-app.include_router(wise.router, prefix="/wise")
-
-
+app.include_router(discord.router, prefix="/discord", dependencies=[Depends(verify_token)])
+app.include_router(ibkr.router, prefix="/ibkr", dependencies=[Depends(verify_token)])
+app.include_router(wise.router, prefix="/wise", dependencies=[Depends(verify_token)])
 
 if __name__ == "__main__":
     import uvicorn
